0a4452ab064fe3f713f1ee9f6bb2984e5c7bf6b5
masterokh <artem.telushkin@yandex.ru>
2023-09-22 23:54:52 +0300

    lab 1,2,3

---
+++ js/sqript.js
@@ -0,0 +1,28 @@
+function initializeKnob(value, className) {
+    var dial = $("." + className);
+    var animationTriggered = false;
+
+    $(document).on("scroll", function() {
+        var topOfDial = dial.offset().top;
+        var bottomOfDial = topOfDial + dial.outerHeight();
+        var bottomOfScreen = $(window).scrollTop() + $(window).height();
+
+        if (bottomOfScreen > topOfDial && $(window).scrollTop() < bottomOfDial && !animationTriggered) {
+            dial.knob({
+                'draw' : function () {
+                    $(this.i).val(this.cv + '%')
+                }
+            });
+
+            $({animatedVal: 0}).animate({animatedVal: value}, {
+                duration: 2000,
+                easing: "swing",
+                step: function() {
+                    dial.val(Math.ceil(this.animatedVal)).trigger("change");
+                }
+            });
+
+            animationTriggered = true;
+        }
+    });
+}
